10bd7947e38b880ebd3df849a37da88d609b8beb
Diego Diaz De La Concha <diegodiazdelaconcha93@gmail.com>
2024-06-14 12:53:34 -0600

    avance al 80% del login, responsive

--- deportiva-x/src/components/Login/Login.jsx
+++ deportiva-x/src/components/Login/Login.jsx
@@ -5,20 +5,26 @@ import './Login.css'
 export default function Login() {
     return (
         <>
-            <section className="container">
-                <article className='title'>
-                    <img src="../../../public/assets/User-icon.png" alt="user-icon" />
-                    <h1 className="">LOGIN</h1>
+            <main>
+                <img src="../../../public/assets/Brand-logo.png" alt="Brand-logo" className='logo' />
+                <article className='container'>
+                    <article className='title'>
+                        <img src="../../../public/assets/User-icon.png" alt="user-icon" />
+                        <h1 className="">LOGIN</h1>
+                        <p className='signin'>Sign in to continue</p>
+                    </article>
+                    <form action="" className='formulario'>
+                        <span>EMAIL</span>
+                        <input type='email' />
+                        <span>PASSWORD</span>
+                        <input type='password' />
+                        <div className='formulario' id='buttons'>
+                            <button type='submit' className='btn'>Login</button>
+                            <button type="button" className='btn'>Sign Up</button>
+                        </div>
+                    </form>
                 </article>
-                <form action="" className='formulario'>
-                    <span>Email</span>
-                    <input type='email' />
-                    <span>Password</span>
-                    <input type='password' />
-                    <button type='submit'>Login</button>
-                    <button type="button">Sign Up</button>
-                </form>
-            </section>
+            </main>
         </>
     )
 }
