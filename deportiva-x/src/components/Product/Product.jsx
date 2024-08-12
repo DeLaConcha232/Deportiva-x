862ded84f59ef61e5bc5f486647132f4226aaf60
rios <arturo_rios@epam.com>
2024-08-12 17:21:04 -0600

    cambios en la api binarios

--- deportiva-x/src/components/Product/Product.jsx
+++ deportiva-x/src/components/Product/Product.jsx
@@ -2,11 +2,8 @@ import './Product.css';
 import Carousel from '../../components/carousel/carousel';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-<<<<<<< HEAD
 import Swal from 'sweetalert2';
-=======
 import Footer from '../Footer/Footer';
->>>>>>> main
 
 export default function Product() {
     const [product, setProduct] = useState(null);
