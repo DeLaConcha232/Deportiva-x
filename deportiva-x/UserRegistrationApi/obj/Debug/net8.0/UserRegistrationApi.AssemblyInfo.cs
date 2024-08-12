679e21145d3e5810368ea5d2c834ef26bb34b087
rios <arturo_rios@epam.com>
2024-08-12 17:22:19 -0600

    Guardando cambios antes de cambiar de rama

--- deportiva-x/UserRegistrationApi/obj/Debug/net8.0/UserRegistrationApi.AssemblyInfo.cs
+++ deportiva-x/UserRegistrationApi/obj/Debug/net8.0/UserRegistrationApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserRegistrationApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a06cd4943c9adc34f0811b56c80b78b138f44bc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ddb420d4d462df0641a185d11fd0500f986b469")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserRegistrationApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserRegistrationApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
