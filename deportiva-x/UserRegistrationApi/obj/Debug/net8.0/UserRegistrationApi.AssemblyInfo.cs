c49e73e0812f57ead3c38d93e67a7091a2d658ab
rios <arturo_rios@epam.com>
2024-08-12 20:42:37 -0600

    Guardando cambios en AssemblyInfo.cs antes de hacer el merge

--- deportiva-x/UserRegistrationApi/obj/Debug/net8.0/UserRegistrationApi.AssemblyInfo.cs
+++ deportiva-x/UserRegistrationApi/obj/Debug/net8.0/UserRegistrationApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserRegistrationApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb88e74bb4fada7a27f83b3fc174ca18fba55096")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4c8c4c30cff57342c3c06f9335cb2b25a6bcade")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserRegistrationApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserRegistrationApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
